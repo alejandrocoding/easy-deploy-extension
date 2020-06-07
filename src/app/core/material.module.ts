0726a0dea622352b22f95abfe267ec4b96a88b6c
Alejandro <alejandrofpo@gmail.com>
2020-06-07 14:45:04 +0200

    refactor: remove unused material modules

--- src/app/core/material.module.ts
+++ src/app/core/material.module.ts
@@ -7,13 +7,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTabsModule } from '@angular/material/tabs';
-
 
 @NgModule({
     imports: [
@@ -23,11 +18,7 @@ import { MatTabsModule } from '@angular/material/tabs';
         MatInputModule,
         MatButtonModule,
         MatIconModule,
-        MatSnackBarModule,
-        MatTooltipModule,
-        MatSlideToggleModule,
         MatDividerModule,
-        MatProgressBarModule,
         MatTabsModule,
     ],
     exports: [
@@ -37,11 +28,7 @@ import { MatTabsModule } from '@angular/material/tabs';
         MatInputModule,
         MatButtonModule,
         MatIconModule,
-        MatSnackBarModule,
-        MatTooltipModule,
-        MatSlideToggleModule,
         MatDividerModule,
-        MatProgressBarModule,
         MatTabsModule,
     ],
 })
